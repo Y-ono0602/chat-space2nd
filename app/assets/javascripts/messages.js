56b2f88d3909b218f2efab669fb2819b0d9c9eb5
Y-ono0602 <ono.yuuki.ono@gmail.com>
2019-10-08 10:41:19 +0900

    ImplementationFinish

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,39 @@
 $(function(){
+  function buildMessage(message){
+    if(message.image == null){
+    var html = `<div class="message">
+                <div class="message__info">
+                <p class="message__info__user">
+                ${message.name}
+                </p>
+                <p class="message__info__date">
+                ${message.time}
+                </p>
+                </div>
+                <p class="message__text">
+                ${message.content}
+                </p>
+                </div>`
+    return html;
+    }
+    else {
+    var html = `<div class="message">
+                <div class="message__info">
+                <p class="message__info__user">
+                ${message.name}
+                </p>
+                <p class="message__info__date">
+                ${message.time}
+                </p>
+                </div>
+                <p class="message__text">
+                ${message.content}
+                </p>
+                <img class="" src="${message.image}" alt="">
+                </div>`
+                return html;
+    }}
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +45,19 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(message){
+      var html = buildMessage(message);
+      $('.main-container__messages').append(html)
+      $(window).scrollTop(300);
+      $('.input-box__text').val('')
+      $('.submit-btn').prop('disabled', false);
+      $('.main-container__messages').removeAttr('disabled');
+      $('.main-container__messages').animate({ scrollTop: $('.main-container__messages')[0].scrollHeight});
+      return false
+    })
+    .fail(function(){
+      alert('エラー');
     })
   })
 });
